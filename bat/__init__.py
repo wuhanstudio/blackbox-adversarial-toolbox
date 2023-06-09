c739e6b15ce1bd053075d2f762d825c1fcd672d9
wuhanstudio <wuhanstudio@qq.com>
2023-06-09 14:19:36 +0100

    [doc] Add cli documentation

--- bat/__init__.py
+++ bat/__init__.py
@@ -1,15 +1,52 @@
 r'''
 Black-box Adversarial Toolbox (BAT) is a python library for **Distrubuted Black-box Attacks** against Deep Learning Cloud Services.
 
+
+## Installation
+
+```
+pip install blackbox-adversarial-toolbox
+```
+
+Then you can use the cli tool `bat` to try distributed black-box attacks.
+
+```
+Usage: bat [OPTIONS] COMMAND [ARGS]...
+
+  The CLI tool for Black-box Adversarial Toolbox (BAT).
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  api      Manage Cloud APIs
+  attack   Manage Attacks
+  example  Manage Examples
+```
+
+<br />
+
 ## bat.apis
 
-Supported Cloud APIs:
+Use `bat api list` to list supported Cloud APIs:
 
-- DeepAPI
-- Google Cloud Vision
-- Imagga
+```
+1 : deepapi     An open-source image classification cloud service for research on black-box adversarial attacks.
+2 : google      Google Cloud Vision AI.
+3 : imagga      Imagga automatic tagging API.
+```
+
+<br />
 
 ## bat.attacks
+
+Use `bat attack list` to list supported attacks:
+
+```
+1 : SimBA               Local Search
+2 : Square Attack       Local Search
+3 : Bandits Atack       Gradient Estimation
+```
 
 Local Search:
 
@@ -19,6 +56,38 @@ Local Search:
 Gradient Estimation:
 
 - Bandits Attack [(Ilyas et al., 2019)](https://arxiv.org/abs/1807.07978)
+
+<br />
+
+## bat.examples
+
+Use `bat example list` to list available examples:
+
+```
+1 : simba_deepapi       SimBA Attack against DeepAPI
+2 : bandits_deepapi     Bandits Attack against DeepAPI
+3 : square_deepapi      Square Attack against DeepAPI
+```
+
+Use `bat example run` to run examples:
+
+```
+Usage: bat example run [OPTIONS] COMMAND [ARGS]...
+
+  Run examples
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  bandits_deepapi  Bandits Attack against DeepAPI
+  simba_deepapi    SimBA Attack against DeepAPI
+  square_deepapi   Square Attack against DeepAPI
+```
+
+For example, the command `bat example run simba_deepapi` initiates a distributed SimBA attack against DeepAPI.
+
+<br />
 
 ## bat.utils
 
